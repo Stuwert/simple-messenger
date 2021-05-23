be7a3047a3023332d3508483470ea3e8898a9bbf
Stuart <stuart.u@hey.com>
2021-05-22 18:47:16 -0600

    Save console

--- messenger-fe/src/utilities/pusherInstance.ts
+++ messenger-fe/src/utilities/pusherInstance.ts
@@ -1,5 +1,6 @@
 import Pusher from "pusher-js";
 
+console.log(process.env.REACT_APP_PUSHER_PUBLIC_KEY!);
 const pusher = new Pusher(process.env.REACT_APP_PUSHER_PUBLIC_KEY!, {
   cluster: process.env.REACT_APP_PUSHER_CLUSTER,
 });
